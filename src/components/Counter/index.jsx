5fcead9369ad2dc6634d845882fc3c2f0383de9e
EvgenStr <juve2910@gmail.com>
2021-05-28 12:21:30 +0300

    add desc  to button stop

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -17,14 +17,14 @@ function Counter() {
       interval = setInterval(autoClick, 1000)
     }
     return () => { clearInterval(interval) }
-  }, [isAuto,counter])
+  }, [isAuto, counter])
 
   return (
     <div className={style.counter}>
       <h1>Counter: {counter}</h1>
       <Controls setSteps={setStep} propStep={step} setDirection={setDirection} direction={direction} />
       <button onClick={() => { setCounter(counter + (direction ? step : -step)) }}> Count</button>
-      <button onClick={()=>{setIsAuto(!isAuto)}}>auto click</button>
+      <button onClick={() => { setIsAuto(!isAuto) }}> {isAuto && 'stop '}auto click </button>
 
     </div>
   )
